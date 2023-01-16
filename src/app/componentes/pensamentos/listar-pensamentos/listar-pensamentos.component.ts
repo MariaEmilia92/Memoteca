6f9a08ad550252b8e12bb00f7a1e41c032ea1437
Maria Emilia Pereira Gomes Martins <mariaemiliapgmartins@hotmail.com>
2023-01-16 20:16:48 -0300

    Add API e Injetar dependencia

--- src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
+++ src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-listar-pensamentos',
@@ -7,24 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentosComponent implements OnInit {
 
-  listaPensamento = [
-    {
-      conteudo: 'Texto 1',
-      autoria: 'Memilia',
-      modelo: 'modelo3'
-    },
-    {
-      conteudo: 'Texto 2',
-      autoria: 'Memilia',
-      modelo: 'modelo2'
-    },
-    {
-      conteudo: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled to make a type specimen book.when an unknown printer took a galley of type and scrambled to make a type specimen book',
-      autoria: 'Memilia',
-      modelo: 'modelo1'
-    } 
-  ];
-listaPensamentos: any;
+  listaPensamento: Pensamento[] = [];
   constructor() { }
 
   ngOnInit(): void {
